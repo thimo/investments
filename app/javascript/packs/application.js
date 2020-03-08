36e067dd862e6f37790971f869ac175454eed5f9
Thimo Jansen <thimo@defrog.nl>
2020-03-08 21:41:54 +0100

    Setup application layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,8 +12,20 @@ require("channels")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
 
 import "controllers"
+
+import "jquery";
+import "bootstrap";
+import "popper.js";
+import "bootstrap-datepicker";
+import "bootstrap-datepicker/dist/locales/bootstrap-datepicker.nl.min";
+
+import "css/application";
+
+// Recursively load all modules within the application folder that end in .js.
+var requireApplication = require.context("../application", true, /\.js$/);
+requireApplication.keys().forEach(requireApplication);
