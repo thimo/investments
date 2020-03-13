8eb03067179568481394622dc0966bb87ce3e9cc
Thimo Jansen <thimo@defrog.nl>
2020-03-13 20:16:42 +0100

    Catch  InvalidAuthenticityToken error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActionController::InvalidAuthenticityToken, with: :invalid_auth_token
+
+  private
+
+    def invalid_auth_token
+      flash_message(:danger, "Je (mogelijk) hebt een verouderde versie van de pagina gebruikt, probeer het nog een keer.")
+      redirect_to back_url
+    end
+
+    def back_url
+      request.referer || root_path
+    end
 end
